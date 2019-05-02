d48d712d22efa35b468fbec9e4d07b13904c92b0
Vũ Thắng <sainttroll99@gmail.com>
2019-05-02 13:48:45 +0700

    homework

---
+++ Homework/some definition/definition.js
@@ -0,0 +1,29 @@
+// // // Boolean refers to a system of logical thought that is used to create true/false statements. A Boolean value expresses 
+// // // a truth value (which can be either true or false). Boolean expressions use the operators AND, OR, XOR and NOT to compare
+// // // values and return a true or false result 
+// // A Boolean expression: 
+// // Examples:- wet && cold 
+// //          - rich || famous
+// //          - !happy
+// //          - walking ^ ridingBus
+
+// // A flow chart is a graphical or symbolic representation of a process. Each step in the process is represented by a different
+// // symbol and contains a short description of the process step. The flow chart symbols are linked together with arrows showing 
+// // the process flow direction
+
+// // Nested conditionals. One conditional can also be nested within another. ... The outer conditional contains two branches. The
+// // second branch (the else from the outer) contains another if statement, which has two branches of its own. Those two branches 
+// // could contain conditional statements as well.
+// // For Examples:
+// let i=10;
+// if(i===10){
+//     if(i<20){
+//         console.log("i is smaller than 20");
+//     }
+//     if(i<15){
+//         console.log("i is smaller than 15 too");
+//     }
+//     else
+//         console.log("i is greater than 20");
+// }
+// console.log(i);
---
+++ Homework/some definition/index.js
@@ -0,0 +1,100 @@
+//1 BMI conditions
+// let height1 = prompt("Enter their height in cm");
+// let weight = prompt("Enter their weight in kg");
+// let height = height1*0.01;
+// let BMI = weight/(height*height);
+// if(BMI<16){
+//     console.log("Severely underweight");
+// }
+// else if (BMI<18.5){
+//     console.log("Underweight");
+// }
+// else if (BMI<25){
+//     console.log("Normal");
+// }
+// else if(BMI<30){
+//     console.log("Overweight");
+// }
+// else {
+//     console.log("Obese");
+// }
+
+//2 Tinh tich
+// let n=prompt("Enter n");
+// let tich = 1;
+// for(let i=1;i<=n;i++){
+//     tich*=i;
+// }
+
+//  console.log(tich);
+
+// 3 Turtle js
+// 1.1
+// color("red")
+//  for(let i=0; i<4; i++){
+// lt(30)
+// fd(100)
+// rt(60)
+// fd(100)
+// rt(120)
+// fd(100)
+// rt(60)
+// fd(100)
+// lt(30)
+// rt(90)
+// color("red")
+// };
+// clear();
+
+// 1.2
+// color("blue")
+// rt(30)
+//  for(let i=0;i<17;i++){
+//    fd(100)
+//  if(i<2){
+//    rt(120)
+//   }
+//  else if(i<7){
+//    rt(72)
+//    }
+
+//  else if(i<11){
+//    color("red")
+//    rt(90)
+//   }
+//  else{
+//    rt(60)
+//    }
+//  };
+// clear();
+
+// Some definitions
+// // // Boolean refers to a system of logical thought that is used to create true/false statements. A Boolean value expresses 
+// // // a truth value (which can be either true or false). Boolean expressions use the operators AND, OR, XOR and NOT to compare
+// // // values and return a true or false result 
+// // A Boolean expression: 
+// // Examples:- wet && cold 
+// //          - rich || famous
+// //          - !happy
+// //          - walking ^ ridingBus
+
+// // A flow chart is a graphical or symbolic representation of a process. Each step in the process is represented by a different
+// // symbol and contains a short description of the process step. The flow chart symbols are linked together with arrows showing 
+// // the process flow direction
+
+// // Nested conditionals. One conditional can also be nested within another. ... The outer conditional contains two branches. The
+// // second branch (the else from the outer) contains another if statement, which has two branches of its own. Those two branches 
+// // could contain conditional statements as well.
+// // For Examples:
+// let i=10;
+// if(i===10){
+//     if(i<20){
+//         console.log("i is smaller than 20");
+//     }
+//     if(i<15){
+//         console.log("i is smaller than 15 too");
+//     }
+//     else
+//         console.log("i is greater than 20");
+// }
+// console.log(i);
